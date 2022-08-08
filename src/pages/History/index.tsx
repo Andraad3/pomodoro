e3f991537806a3ce206e2f92dbeb03491c184030
Andraad3 <joao.andraade25@gmail.com>
2022-08-07 22:25:06 -0300

    first commit

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,48 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Stauts</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 semanas</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 semanas</td>
+              <td>
+                <Status statusColor="yellow">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 semanas</td>
+              <td>
+                <Status statusColor="red">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
